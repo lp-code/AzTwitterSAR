ec504d4ec2f5d4058bcc9de6d4a169ed25e870d0
lp-code <lp-code@users.noreply.github.com>
2019-11-23 23:13:58 +0100

    Replace MS Excel-access package by a third-party one.

--- ProcessTweets2/Program.cs
+++ ProcessTweets2/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace ProcessTweets
 {
@@ -12,63 +13,35 @@ namespace ProcessTweets
     {
         static void Main(string[] args)
         {
-            const string fileName = "C:\\User\\lpesch\\test.xlsx";
-            Console.WriteLine(fileName);
-            Console.ReadLine();
-            //Create COM Objects. Create a COM object for everything that is referenced
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbooks xlWorkbooks = xlApp.Workbooks;
-            Excel.Workbook xlWorkbook = xlWorkbooks.Open(fileName); // @"C:\Users\lpesch\Private\RKH\TwitterSAR\Tweets\1_ProcessedScript\vicinitas_user_tweets_vest_scoring_layout.xlsx");
-            Excel._Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            int i = 2;
-            int nonzeroScores = 0;
-            while (xlRange.Cells[i, 4] != null && ((Excel.Range)xlRange.Cells[i, 4]).Value2 != null)
+            const string fileName = @"C:\Users\lpesch\Private\RKH\TwitterSAR\Tweets\1_ProcessedScript\vicinitas_user_tweets_vest_scoring_layout.xlsx";
+            var fi = new FileInfo(fileName);
+            using (var package = new ExcelPackage(fi))
             {
-                // run the scoring function, output if nonzero
-                float score = AzTwitterSar.ProcessTweets.AzTwitterSarFunc.ScoreTweet(((Excel.Range)xlRange.Cells[i, 4]).Value2.ToString(), out string _);
-                ((Excel.Range)xlRange.Cells[i, 2]).Value2 = score;
-                if (score > 0)
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
+                var rowCount = worksheet.Dimension.Rows;
+
+                int nonzeroScores = 0;
+                for (int row = 2; row <= rowCount; row++)
                 {
-                    Console.WriteLine(((Excel.Range)xlRange.Cells[i, 4]).Value2.ToString());
-                    Console.WriteLine($"Score: {score}");
-                    nonzeroScores++;
+                    if (worksheet.Cells[row, 4] != null && (worksheet.Cells[row, 4]).Value != null)
+                    {
+                        // run the scoring function, output if nonzero
+                        float score = AzTwitterSar.ProcessTweets.AzTwitterSarFunc.ScoreTweet((worksheet.Cells[row, 4]).Value.ToString(), out string _);
+                        worksheet.Cells[row, 2].Value = score;
+                        if (score > 0)
+                        {
+                            Console.WriteLine(worksheet.Cells[row, 4].Value.ToString());
+                            Console.WriteLine($"Score: {score}");
+                            nonzeroScores++;
+                        }
+                    }
                 }
-                i++;
+                Console.WriteLine($"Found {nonzeroScores} tweets with nonzero score.");
+                package.Save();
             }
-            Console.WriteLine($"Found {nonzeroScores} tweets with nonzero score.");
 
-            xlWorkbook.Save();
-
-            //cleanup
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            //rule of thumb for releasing com objects:
-            //  never use two dots, all COM objects must be referenced and released individually
-            //  ex: [somthing].[something].[something] is bad
-
-            //release com objects to fully kill excel process from running in the background
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            //close and release
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
-            Marshal.ReleaseComObject(xlWorkbooks);
-
-            //quit and release
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
-
-
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Processing finished!");
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
