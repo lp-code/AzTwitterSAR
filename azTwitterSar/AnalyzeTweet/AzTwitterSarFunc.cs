c338005db14584eb7f14dc7ad9e0b998490b2977
lp-code <lp-code@users.noreply.github.com>
2020-01-09 21:08:45 +0100

    Clean up a double occurence of an irrelevant word.

--- azTwitterSar/AnalyzeTweet/AzTwitterSarFunc.cs
+++ azTwitterSar/AnalyzeTweet/AzTwitterSarFunc.cs
@@ -60,8 +60,7 @@ namespace AzTwitterSar.ProcessTweets
             "opprette", // rette
             "deretter", // rette
             "rettelse", // rette
-            "korskirkeallmenning", // Kors
-            "korskirkeveiten",
+            "korskirke",
             "rossleitet", // leite
             "korsrygg",
         };
